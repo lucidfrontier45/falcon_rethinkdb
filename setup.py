c1c01e822dd3edf9dc8152a7fbd6bcd7331cda6b
Shiqiao Du <lucidfrontier.45@gmail.com>
2016-09-23 23:18:58 +0900

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='falcon_rethinkdb',
-    version='0.1.1',
+    version='0.1.3',
     packages=['falcon_rethinkdb'],
     url='https://github.com/lucidfrontier45/falcon_rethinkdb',
     license='Apache License version 2',
